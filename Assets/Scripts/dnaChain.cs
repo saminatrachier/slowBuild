ce4a0aa3dfd4da9c37c327d882f67f4538807ddc
Samina Trachier <st3225@nyu.edu>
2019-09-19 17:05:36 -0400

    added scripts to prefabs

---
+++ Assets/Scripts/dnaChain.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//usage: put this on my DNA prefabs to spawn the prefabs randomly 
+//also create matching pairs for 
+//basically assign prefabs
+
+public class dnaChain : MonoBehaviour
+{
+    List<GameObject> prefabList = new List<GameObject>();
+
+    public GameObject PrefabA;
+
+    public GameObject PrefabT;
+
+    public GameObject PrefabC;
+
+    public GameObject PrefabG;
+    // Start is called before the first frame update
+    void Start()
+    {
+        prefabList.Add(PrefabA);
+        prefabList.Add(PrefabC);
+        prefabList.Add(PrefabG);
+        prefabList.Add(PrefabT);
+
+        int prefabIndex = UnityEngine.Random.Range(0, 4);
+        Instantiate(prefabList[prefabIndex]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/dnaSpawn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Usage: Spawn the prefabs randomly in a straight line for the DNA Chain
+//
+public class dnaSpawn : MonoBehaviour
+{
+    public dnaChain myDNAPrefab;
+
+    public int maxDNACount = 100; //how many DNA should we be able to make?
+    List<dnaChain> myDNAList = new List<dnaChain>(); //list of all the DNA that spawns
+    // Start is called before the first frame update
+    void Start()
+    {
+        int currentDNACount = 0;
+        while (currentDNACount < maxDNACount)
+        {
+            dnaChain newDNAClone = (dnaChain) Instantiate(myDNAPrefab, transform.position, transform.rotation);
+            myDNAList.Add(newDNAClone);
+            currentDNACount++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    //player input:
+    //if player presses the correct key then another key should spawn
+    
+    
+}
