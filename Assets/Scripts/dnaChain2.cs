c5980d60cdcbf11f39e319a62e1e17cf7eb29d72
Samina Trachier <st3225@nyu.edu>
2019-09-23 23:25:15 -0400

    made 2 player system

---
+++ Assets/Scripts/dnaChain2.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//usage: put this on my DNA prefabs to spawn the prefabs randomly 
+//also create matching pairs for 
+//basically assign prefabs
+
+public class dnaChain2 : MonoBehaviour
+{
+    //prefabs for the player input
+    public GameObject prefabA;
+
+    public GameObject prefabT;
+
+    public GameObject prefabC;
+
+    public GameObject prefabG;
+    
+    //time left for player objects to appear
+    public float timeLeft = 0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Instantiate(prefabA, transform.position, transform.rotation);
+            this.GetComponent<Transform>().Translate(new Vector3(0, 2f));
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Instantiate(prefabG, transform.position, transform.rotation);
+            this.GetComponent<Transform>().Translate(new Vector3(0, 2f));
+
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Instantiate(prefabC, transform.position, transform.rotation);
+            this.GetComponent<Transform>().Translate(new Vector3(0, 2f));
+
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Instantiate(prefabT, transform.position, transform.rotation);
+            this.GetComponent<Transform>().Translate(new Vector3(0, 2f));
+
+        }
+        
+    
+        
+    }
+}
