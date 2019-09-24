4add686153e89e76daecfa76147b0d0d76fc99fa
Samina Trachier <st3225@nyu.edu>
2019-09-24 11:52:19 -0400

    Fixed Player's Scores

---
+++ Assets/Scripts/ScoreText2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//usage: create a UI score counter for player 1 when they make a correct match
+
+public class ScoreText2 : MonoBehaviour
+{
+    private Text text2;
+
+    public static int Score2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        text2 = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text2.text = Score2.ToString();
+    }
+}
