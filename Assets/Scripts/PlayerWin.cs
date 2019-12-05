4d4fc97af4cecc55b2875a5335b3b6b901c01186
Maddy <ms9642@nyu.edu>
2019-12-05 15:09:28 -0500

    End scene now says winner

---
+++ Assets/Scripts/PlayerWin.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerWin : MonoBehaviour
+{
+
+    public TextMeshProUGUI endText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dnaChain.winner == true)
+        {
+            endText.text = "Player 1 has won!";
+        }
+        else if (dnaChain2.winner2 == true)
+        {
+            endText.text = "Player 2 has won!";
+        }
+        else
+        {
+            endText.text = "Yeet!";
+        }
+    }
+}
